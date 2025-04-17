0051d1f9ad9ea12098169936081717dd83ef4352
jiwonwonkk <jiwon0935@naver.com>
2025-04-17 21:35:57 +0900

    ServerMain.java

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -5,14 +5,40 @@ import java.io.IOException;
 import server.network.GameServer;
 
 public class ServerMain {
-	
-	public static void main(String[] args) {
-		GameServer server = new GameServer();
-		try {
-			server.start(3000);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+   
+   public static void main(String[] args) {
+      JFrame frame = new JFrame("게임 서버");
+      frame.setBounds(200, 200, 800, 200);
+      
+      String str = JOptionPane.showInputDialog("설정 파일 위치");
+      if (str != null && !str.isBlank()) {
+         File file = new File(str);
+         if (file.exists()) {
+            GameService.accountFilePath = str;
+         }
+      }
+      
+      frame.addWindowListener(new WindowAdapter() {
+         @Override
+         public void windowClosing(WindowEvent e) {
+            System.out.println("SERVER EXIT");
+            System.exit(0);
+         }
+      });
+      
+      JLabel lblFilePath = new JLabel(GameService.accountFilePath);
+      lblFilePath.setSize(200, 200);
+      frame.add(lblFilePath);
+      
+      frame.setVisible(true);
+      GameServer server = new GameServer();
+      try {
+         server.start(3000);
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      
+      frame.setVisible(true);
+   }
 }
